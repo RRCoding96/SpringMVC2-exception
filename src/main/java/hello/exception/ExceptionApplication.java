fec246cd31fb45793c76d692a634cb356cc767b3
김윤욱 <reznov03048@gmail.com>
2023-04-14 17:01:39 +0900

    dffd

--- src/main/java/hello/exception/ExceptionApplication.java
+++ src/main/java/hello/exception/ExceptionApplication.java
@@ -3,7 +3,7 @@ package hello.exception;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication////
 public class ExceptionApplication {
 
 	public static void main(String[] args) {
